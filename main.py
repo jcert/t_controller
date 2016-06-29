728c80ca67e515550c0b02774202b23257442950
jcert <jgcertorio@uol.com.br>
2016-06-29 16:26:41 -0400

    stared the code

---
+++ main.py
@@ -0,0 +1,55 @@
+import RPIO
+import threading
+
+
+
+
+
+
+
+class encoder(pinA, pinB, pinC):
+	ENCODER_TICKS_TURN = 20
+
+	def update()
+		read A
+		read b
+		is different state from before?
+		FSM magic -> which direction it moved to
+		add that to total distance
+	update estimated velocity
+
+
+			  get_speed()
+			  get_dist()
+
+
+
+def wheelsFunction():
+
+
+
+
+
+
+
+
+def main():
+
+
+
+
+
+wheels= threading.Thread(target=wheelsFunction)
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
