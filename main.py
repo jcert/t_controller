73f0fa86dbc3cb8c1b9e6a6e5a58764b206f7845
jcert <jgcertorio@uol.com.br>
2016-06-30 17:24:56 -0400

    started the encoders, obj and thread, and the imu obj

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import RPIO
 import threading
-
+import encoder
+import imu
+import esc
+import motor
 
 def wheelsFunction():
 
@@ -8,12 +11,19 @@ def wheelsFunction():
 
 
 def main():
+	my_imu = IMU_device(0x19,0x1E,0x6B)#their order might be wrong, check later
+	my_encoder1 = encoder(0,0,0)#find the real pins the encoder will be attached to
+	my_encoder2 = encoder(0,0,0)#find the real pins the encoder will be attached to
+	
+	#this will keep checking the encoders for position change
+	#should we worry about debouncing the encoder?
+	encoder_thread1 =  threading.Thread(target=my_encoder1.update)
+	encoder_thread2 =  threading.Thread(target=my_encoder2.update)
+	encoder_thread1.start()
+	encoder_thread2.start()
+
 
 	wheels= threading.Thread(target=wheelsFunction)
-
-
-
-
 
 
 
