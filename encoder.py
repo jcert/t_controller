46a01c8cd8daae7ae0c99c73461bdd3347456d3a
jcert <jgcertorio@uol.com.br>
2016-06-30 13:22:48 -0400

    corrected some stuff, such as instance variables, still need to do the pin config part

--- encoder.py
+++ encoder.py
@@ -5,21 +5,18 @@ class encoder:
 	ENCODER_TICKS_TURN = 20 #number of ticks per full rotation of the shaft
 	DEL_TIME = 1 #in seconds
 	#pins of the encoder
-	A = 0 
-	B = 0 
-	C = 0
-	state=0
-	distance=0
-	speed=0
 	def __init__(self,pinA, pinB, pinC):
-		A = pinA
-		B = pinB
-		C = pinC
+		self.A = pinA
+		self.B = pinB
+		self.C = pinC
+
 		RPIO.setup(7, RPIO.IN, pull_up_down=RPIO.PUD_UP)
-		S1 = GPIO.input(A)
-		S2 = GPIO.input(B)
-		state = S1 + 2*S2
-		start_time = time.clock()
+		
+
+		self.S1 = GPIO.input(A)
+		self.S2 = GPIO.input(B)
+		self.state = S1 + 2*S2
+		self.start_time = time.clock()
 
 	def fsm(old,new):
 		table = [0,-1,1,0,
@@ -29,26 +26,24 @@ class encoder:
 		return table[4*old+new]       
 	
 	def update():
-		newS1 = RPIO.input(A)
-		newS2 = RPIO.input(B)
-		new_state = newS1 + 2*newS2 		
-		rotation = self.fsm(state,new_state) #checks if the state is different from before
+		self.newS1 = RPIO.input(A)
+		self.newS2 = RPIO.input(B)
+		self.new_state = newS1 + 2*newS2 		
+		self.rotation = self.fsm(state,new_state) #checks if the state is different from before
 		
-		distance+=rotation
+		self.distance+=self.rotation
 
-		if time.clock()-DEL_TIME>start_time:
-			speed = steps_for_speed/DEL_TIME
-			steps_for_speed = 0
-			start_time = time.clock()
+		if time.clock()-DEL_TIME>self.start_time:
+			self.speed = self.steps_for_speed/DEL_TIME
+			self.steps_for_speed = 0
+			self.start_time = time.clock()
 		else: 
-			steps_for_speed += rotation
-
-		speed =0 #how to estimate the velocity?
+			self.steps_for_speed += self.rotation
 
 	def get_speed():
-		return speed
+		return self.speed
 	def get_dist():
-		return distance
+		return self.distance
 
 
 
