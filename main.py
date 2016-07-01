8fb646ca3bc4c5f4f067f07feb5281c59bcc9252
jcert <jgcertorio@uol.com.br>
2016-07-01 13:02:23 -0400

    just to be up to date with encoder.py, fixed the encoders stuff

--- main.py
+++ main.py
@@ -13,8 +13,11 @@ def wheelsFunction():
 def main():
 	my_imu = IMU_device(0x19,0x1E,0x6B)#their order might be wrong, check later
 	#also, if more wheels, create more encoder objs, so each is measured by one
-	my_encoder1 = encoder(0,0,0)#find the real pins the encoder will be attached to
-	my_encoder2 = encoder(0,0,0)#find the real pins the encoder will be attached to
+	encoder_group = encoders(pinA,pinB)#obj that handles all encoders
+	my_encoder1 = encoder_group.encoder(0)#find the real pins the encoder will be attached to
+	my_encoder2 = encoder_group.encoder(0)#find the real pins the encoder will be attached to
+	my_encoder3 = encoder_group.encoder(0)#find the real pins the encoder will be attached to
+	my_encoder4 = encoder_group.encoder(0)#find the real pins the encoder will be attached to
 	
 	#this will keep checking the encoders for position change
 	#should we worry about debouncing the encoder?
