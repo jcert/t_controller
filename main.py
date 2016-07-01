544a374e38a8a8612e70c5771413d16bf435d3c8
jcert <jgcertorio@uol.com.br>
2016-07-01 13:19:58 -0400

    more encoder related, one thread for each encoder

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ import motor
 def wheelsFunction():
 
 
-
-
 def main():
 	my_imu = IMU_device(0x19,0x1E,0x6B)#their order might be wrong, check later
 	#also, if more wheels, create more encoder objs, so each is measured by one
@@ -20,15 +18,17 @@ def main():
 	my_encoder4 = encoder_group.encoder(0)#find the real pins the encoder will be attached to
 	
 	#this will keep checking the encoders for position change
-	#should we worry about debouncing the encoder?
+	#should we worry about debouncing the encoder?should we group the updates in some way? probably
 	encoder_thread1 =  threading.Thread(target=my_encoder1.update)
 	encoder_thread2 =  threading.Thread(target=my_encoder2.update)
+	encoder_thread3 =  threading.Thread(target=my_encoder3.update)
+	encoder_thread4 =  threading.Thread(target=my_encoder4.update)
 	encoder_thread1.start()
 	encoder_thread2.start()
-
+	encoder_thread3.start()
+	encoder_thread4.start()
 
 	wheels= threading.Thread(target=wheelsFunction)
-
 
 
 if __name__ == "__main__":
