dcf33645ee10c0612dd02798fda0b147228a9318
jboco <jboco@students.pgcc.edu>
2016-07-04 17:33:03 -0400

    Update esc.py
    found it online. not sure if we will need the debug functions and kv variable.

---
+++ esc.py
@@ -0,0 +1,163 @@
+class motor(object):
+   
+    def __init__(self, name, pin, kv=1000, WMin=0, WMax=100, debug=True, simulation=True):
+        self.name = name
+        self.powered = False
+        self.simulation = simulation
+        self.__pin = pin
+        self.__kv = kv
+        self.setWLimits(WMin, WMax)
+        self.setDebug(debug)
+
+        self.__W = self.__WMin
+        self.__Wh = 10
+
+        try:
+            from RPIO import PWM
+            self.__IO = PWM.Servo()
+        except ImportError:
+            self.simulation = True
+
+    def setDebug(self, debug):
+        self.__debug = debug
+        #if self.__debug:
+            #self.__logger.setLevel(logging.DEBUG)
+        #else:
+            #self.__logger.setLevel(logging.WARNING)
+
+    def getDebug(self):
+        return self.__debug
+
+    def setPin(self, pin):
+        "set the pin for each motor"
+        self.__pin = pin
+
+    def setKv(self, kv):
+        "set the kv for each motor"
+        self.__kv = kv
+
+    def setWLimits(self, WMin, WMax):
+        "set the pin for each motor"
+        if WMin < 0:
+            WMin = 0
+        self.__WMin = WMin
+        if WMax > 100:
+            WMax = 100
+        self.__WMax = WMax
+
+    def saveWh(self):
+        "Save Wh = current W%"
+        self.__Wh = self.__W
+
+    def setWh(self):
+        "Sets current W% =Wh"
+        self.__W = self.__Wh
+        self.setW(self.__W)
+
+    def getWh(self):
+        "returns current W% =Wh"
+        return self.__Wh
+
+    def start(self):
+        "Run the procedure to init the PWM"
+        if not self.simulation:
+            try:
+                from RPIO import PWM
+                self.__IO = PWM.Servo()
+                self.powered = True
+                #TODO Decide How to manage the WMax < 100
+                #to keep anyhow the throttle range 0-100
+            except ImportError:
+                self.simulation = True
+                self.powered = False
+
+    def stop(self):
+        "Stop PWM signal"
+
+        self.setW(0)
+        if self.powered:
+            self.__IO.stop_servo(self.__pin)
+            self.powered = False
+
+    def increaseW(self, step=1):
+        "increases W% for the motor"
+
+        self.__W = self.__W + step
+        self.setW(self.__W)
+
+    def decreaseW(self, step=1):
+        "decreases W% for the motor"
+
+        self.__W = self.__W - step
+        self.setW(self.__W)
+
+    def getW(self):
+        "retuns current W%"
+        return self.__W
+
+    def setW(self, W):
+        "Checks W% is between limits than sets it"
+
+        PW = 0
+        self.__W = W
+        if self.__W < self.__WMin:
+            self.__W = self.__WMin
+        if self.__W > self.__WMax:
+            self.__W = self.__WMax
+        PW = (1000 + (self.__W) * 10)
+        # Set servo to xxx us
+        if self.powered:
+            self.__IO.set_servo(self.__pin, PW)
+
+
+from motor import motor
+
+mymotor = motor('m1', 17, simulation=False)
+#where 17 is  GPIO17 = pin 11
+
+print('***Disconnect ESC power')
+print('***then press ENTER')
+res = raw_input()
+mymotor.start()
+mymotor.setW(100)
+
+#NOTE:the angular motor speed W can vary from 0 (min) to 100 (max)
+#the scaling to pwm is done inside motor class
+print('***Connect ESC Power')
+print('***Wait beep-beep')
+
+print('***then press ENTER')
+res = raw_input()
+mymotor.setW(0)
+print('***Wait N beep for battery cell')
+print('***Wait beeeeeep for ready')
+print('***then press ENTER')
+res = raw_input()
+print ('increase > a | decrease > z | save Wh > n | set Wh > h|quit > 9')
+
+cycling = True
+try:
+    while cycling:
+        res = raw_input()
+        if res == 'a':
+            mymotor.increaseW()
+        if res == 'z':
+            mymotor.decreaseW()
+        if res == 'n':
+            mymotor.saveWh()
+        if res == 'h':
+            mymotor.setWh()
+        if res == '9':
+            cycling = False
+finally:
+    # shut down cleanly
+    mymotor.stop()
+    print ("well done!")
+
+
+
+
+
+
+
+
