38ebea1aad264b9a8b886f993a9043647dbb880b
jcert <jgcertorio@uol.com.br>
2016-07-13 13:46:02 -0400

    new files for tests

---
+++ tests/encoder_test.py
@@ -0,0 +1,24 @@
+import encoder
+import time
+
+
+component_measure = encoders(20,21) #pins A and B
+component    =    component_measure.encoder(16) #pin C
+
+
+c=0
+while true:
+	component_measure.update()
+	sleep(0.01)
+	c+=1
+	if c > 500:
+		c=0
+		print component.get_dist() 
+	pass
+
+
+
+
+
+
+
