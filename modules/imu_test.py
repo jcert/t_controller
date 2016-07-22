8062bc08df6cf3d89b6b0731ee1f652809288f6f
jcert <jgcertorio@uol.com.br>
2016-07-22 17:49:24 -0400

    changes to test imu

--- modules/imu_test.py
+++ modules/imu_test.py
@@ -5,11 +5,13 @@ import time
 
 x = imu.IMU_device(0x19,0x6b,0x1e)
 
-time.sleep(1)
 
-print x.accel.read()
-print x.gyro.read()
-print x.comp.read()
+
+for i in range(0,20):
+	time.sleep(0.3)
+	print x.accel.read()
+	print x.gyro.read()
+	print x.comp.read()
 
 
 
