ed591f986d7769e0c0e38c9492f4915063acf004
joubel <jcertorio@uol.com.br>
2016-07-25 22:50:48 +0000

    fix bugs in motor.py

--- modules/hardware/motor.py
+++ modules/hardware/motor.py
@@ -5,17 +5,19 @@ import socket
 import time
 import string
 import sys
+import RPIO
 import RPIO.PWM as PWM
 import math
 import threading
-RPIO.setmode(RPIO.BCM)
+
 
 
 
 class motor: #has all motors in one class, what about the 2 wheels and caster? would've been better to make a class for motors, one for driver (w/ 2 motors in) and then a super-class w/ 2 driver in it.
-	def __init__()
-	PWM.setup(1)#put this in def __init__():
-	PWM.init_channel(1, 3000)
+	def __init__():
+		RPIO.setmode(RPIO.BCM)
+		PWM.setup(1)#put this in def __init__():
+		PWM.init_channel(1, 3000)
 	class driver:
 		def __init__(self):
 			self.motor_list=[]
@@ -31,31 +33,13 @@ class motor: #has all motors in one class, what about the 2 wheels and caster? w
 			RPIO.output(pin1, False)
 			RPIO.output(pin2, False)
 		
-		def set_velocity(pwmotor,motor_id)
+		def set_velocity(pwmotor,motor_id):
 			pins = self.motor_list[motor_id]
-			if pwmotor>0
-			PWM.add_channel_pulse(1,pins[0],0,pwmotor)
-			PWM.add_channel_pulse(1,pins[1],0,0)
-			else
-			PWM.add_channel_pulse(1,pins[0],0,0)
-			PWM.add_channel_pulse(1,pins[1],0,pwmotor)
+			if pwmotor>0:
+				PWM.add_channel_pulse(1,pins[0],0,pwmotor)
+				PWM.add_channel_pulse(1,pins[1],0,0)
+			else:
+				PWM.add_channel_pulse(1,pins[0],0,0)
+				PWM.add_channel_pulse(1,pins[1],0,pwmotor)
 
-	def update_velocity(direction1,direction2, direction3,direction4,pwmotor1, pwmotor2, pwmotor3, pwmotor4):
-		
-		if direction1 == "f":
-			clockwise(in1_pinA,in1_pinB,pwmotor1)
-		else:
-			counter_clockwise(in1_pinA,in1_pinB,pwmotor1)
-		if direction2== "f":
-			clockwise(in2_pinA,in2_pinB,pwmotor2)
-		else: 
-			counter_clockwise(in2_pinA,in2_pinB,pwmotor2)
-		if direction3== "f":
-			clockwise(in3_pinA,in3_pinB,pwmotor3)
-		else:
-			counter_clockwise(in3_pinA,in3_pinB,pwmotor3)
-		if direction4== "f":
-			clockwise(in4_pinA,in4_pinB,pwmotor4)
-		else:
-			counter_clockwise(in4_pinA,in4_pinB,pwmotor4)
-		 
+	
